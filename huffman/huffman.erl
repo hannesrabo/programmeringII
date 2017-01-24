a1f5d92980825aeb2d289e92b95a53c4e6ff9f52
hannesrabo <hannes.rabo@gmail.com>
2017-01-24 10:52:07 +0100

    Added function to create the huffman tree

--- huffman/huffman.erl
+++ huffman/huffman.erl
@@ -17,19 +17,21 @@ text() ->
     % Text = decode(Seq, Decode).
 
 % %Creates the huffman tree from frequencies in the text
-% tree(Sample) ->
-%     Freq = freq(Sample),
-%     huffman(Freq).
+tree(Sample) ->
+    Freq = freq(Sample),
+    huffman(Freq).
 
-%Find the frequency of the chars in the sample.
+%Find the frequency of the chars in the sample (external).
 freq(Sample) ->
     freq(Sample, []).
 
+%Calculate frequency of all chars in list (internal)
 freq([], Freq) ->
     Freq;
 freq([Char|Rest], Freq) ->
     freq(Rest, freq_add(Char, Freq)).
 
+%Adds one to the frequencycounter of the supplied char
 freq_add(Char, []) ->
     [{Char, 1}];
 freq_add(Char, [{Char, Occ} | Rest]) ->
@@ -37,25 +39,39 @@ freq_add(Char, [{Char, Occ} | Rest]) ->
 freq_add(Char, [Head|Rest]) ->
     [Head|freq_add(Char, Rest)].
 
-
+%This function cunstructs the huffman tree from a list of frequencies
 huffman(Freq) ->
-    huffman_order(Freq, []).
+    huffman_construct_tree(huffman_order(Freq, [])).
 
+%Construct the huffman tree from a sorted list of leafs
+%Adds the two first elements together and then their values. This is inserted at the correct position in the tree
+huffman_construct_tree([E1 = {_, V1}, E2 = {_, V2}]) ->
+    [{{E1, E2}, V1+V2}];
+%This makes the single element appear last in the list (not wanted?)
+% huffman_construct_tree([{_, V1} = H1,{_, V2} =  H2, T]) ->
+%     [{{H1, H2}, V1 + V2}, T];
+huffman_construct_tree([{_, V1} = H1,{_, V2} =  H2 | T]) ->
+    huffman_construct_tree(huffman_insert({{H1, H2}, V1 + V2}, T)).
+
+%Sort the leafs.
 huffman_order([], Freq_sorted) ->
     Freq_sorted;
 huffman_order([Freq_h|Freq_t], Freq_sorted) ->
     huffman_order(Freq_t, huffman_insert(Freq_h, Freq_sorted)).
 
+%Insert a leaf at the correct location.
 huffman_insert(Leaf, []) ->
     [Leaf];
-huffman_insert({_, L_Val} = Leaf, [{_, E_Val} = H | T]) when L_Val < E_Val->
+huffman_insert({_, L_Val} = Leaf, [{_, E_Val} = H | T]) when L_Val > E_Val->
     [H | huffman_insert(Leaf, T)];
 huffman_insert(Leaf, Trailing) ->
     [Leaf | Trailing].
 
 
-
-% encode_table(_Tree) ->
+% encode_table({C, _})
+%
+% encode_table([Head | Tree]) ->
+%
 %     .
 %
 % decode_table(_Tree) ->
